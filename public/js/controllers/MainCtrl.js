5702ff833361f7de8dee22f5117fb84592f8eefa
jc29 <jc29@princeton.edu>
2015-02-12 15:58:54 -0500

    first push

---
+++ public/js/controllers/MainCtrl.js
@@ -0,0 +1,6 @@
+// public/js/controllers/MainCtrl.js
+angular.module('MainCtrl', []).controller('MainController', function($scope) {
+
+	$scope.tagline = 'This is just a test. Other pages were part of a tutorial';	
+
+});
